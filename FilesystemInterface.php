eae619f136f145a9a4b9d248ef6c22c3c2f3501c
Amy Stephen <AmyStephen@gmail.com>
2014-01-03 17:10:44 -0600

    Copyright Year

--- FilesystemInterface.php
+++ FilesystemInterface.php
@@ -3,7 +3,7 @@
  * Filesystem Adapter Interface
  *
  * @package    Filesystem
- * @copyright  2013 Common Api. All rights reserved.
+ * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
 namespace CommonApi\Filesystem;
@@ -12,9 +12,9 @@ namespace CommonApi\Filesystem;
  * Filesystem Adapter Interface
  *
  * @package    Filesystem
- * @copyright  2013 Common Api. All rights reserved.
+ * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
- * @since      0.1
+ * @since      1.0
  */
 interface FilesystemInterface
 {
